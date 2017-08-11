523674745695a91b1a50af7bd403715a3e02033d
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-08-10 21:40:38 -0700

    Scrolling the background
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

---
+++ Assets/_Scripts/BGScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float tileSize;
+
+	private Vector3 startPosition;
+
+	void Start() {
+		startPosition = transform.position;
+	}
+	
+	void Update() {
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+		transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
